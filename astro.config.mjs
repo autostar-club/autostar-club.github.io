8db0ec0e4b904174d22ff3432866fc0730b3bdbc
Olivier Barais <olivier.barais@gmail.com>
2026-07-29 17:50:46 +0200

    update with data

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import vue from '@astrojs/vue';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   legacy: {
     collectionsBackwardsCompat: true,
   },
-  integrations: [vue()],
+  integrations: [vue(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
